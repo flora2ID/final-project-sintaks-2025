b6c83f978042339b1ffcd25a1e6f85aae858ee44
flora2ID <floraenjellina31858@gmail.com>
2025-05-28 23:41:34 +0700

    tes

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from './HeroSection'
 
 function AboutUsPage() {
   return (
     <>
-    
+    <HeroSection/>
     </>
   )
 }
--- src/pages/AboutUsPage/HeroSection.jsx
+++ src/pages/AboutUsPage/HeroSection.jsx
@@ -18,7 +18,6 @@ function HeroSection() {
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
       </p>
-      <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
