b8a601d921c2660e9c7090a2a17ea5905aacf0e5
flora2ID <floraenjellina31858@gmail.com>
2025-05-28 23:33:08 +0700

    tes 1

---
+++ src/pages/AboutUsPage/HeroSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <>
+     <div
+  class="hero min-h-screen"
+  style={{
+    background :
+     "(public/images/About/Kain-Songket.png);",
+  }}
+>
+  <div class="hero-overlay"></div>
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">About Sumatera Sutra</h1>
+      <p class="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default HeroSection
